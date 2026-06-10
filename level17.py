543477c28d85a1330986ee7505f23c182e336061
sandrobatkuashvili <sandrobatkuashvili@gmail.com>
2026-06-10 16:44:18 +0400

    Add files via upload

---
+++ level17.py
@@ -0,0 +1,20 @@
+num1 = input("Enter your number ")
+num2 = input("Enter your same number ")
+
+
+for i in range(20):
+    print("Sandro")
+
+while num2 != num1:
+    print("WRONG!")
+    num2 = input("Enter your same number ")
+
+
+
+# 2)მომხმარებელს შემოატანინე ორი რიცხვი და დაპრინტე რიცხვები ერთიდან მეორემდე
+
+number1 = int(input("Enter your first number "))
+number2 = int(input("Enter your second number "))
+
+for i in range(number1, number2):
+    print(i)
